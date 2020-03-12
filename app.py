be439de5b18b4fcc0ac04d7232d52cb0fad6dcec
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-03-12 21:29:45 +0000

    Add db in ext and initialise it app

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from flask import Flask
 from flask_security import PeeweeUserDatastore, Security
 from peewee import PostgresqlDatabase
 
+from todo_list.ext import db
+
 app: Flask = Flask(__name__.split('.')[0])
 
 # Load the config
 app.config.from_pyfile(Path(f'./config/common.py'))
 app.config.from_pyfile(Path(f'./config/{os.environ.get("APP_ENVIRONMENT", default="dev").lower()}.py'))
 
-# Initialise the peewee orm
-db = PostgresqlDatabase(
+# Initialise the peewee orm for postgres
+postgres_db = PostgresqlDatabase(
     app.config.get('APP_NAME'),
     user=app.config.get('DB_USERNAME'),
     password=app.config.get('DB_PASSWORD'),
     host='10.1.0.9', port=5432
 )
+db.init(postgres_db)
 
 
 class UserModel(db):
@@ -33,5 +36,5 @@ class UserRoleModel(db):
 
 
 # Setup Flask-Security
-user_data_store = PeeweeUserDatastore(db, UserModel, RoleModel, UserRoleModel)
+user_data_store = PeeweeUserDatastore(postgres_db, UserModel, RoleModel, UserRoleModel)
 security = Security(app, user_data_store)
